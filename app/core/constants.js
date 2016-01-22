94298feb18849914ab169f88462cb333c1ee50d1
Omar Garcia <isc.omargarcia@gmail.com>
2016-01-22 00:40:33 -0600

    Introducir una constante de angular para variables usadas en toda la aplicacion?

---
+++ app/core/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var APP_CONSTANTS = {
+    APP_NAME: 'IVAMATICO',
+    HEADER_TITLE: 'IVAMATICO',
+    HEADER_SUBTITLE: 'Calcula o desgloza el IVA'
+}
+
+module.exports = APP_CONSTANTS;
