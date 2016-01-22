691801d39a1cdfc8d4dab08e70de32739585e0ed
Omar Garcia <isc.omargarcia@gmail.com>
2016-01-22 01:10:16 -0600

    Agregar la directiva a la aplicacion

--- app/calculador/index.js
+++ app/calculador/index.js
@@ -5,3 +5,4 @@ var ivamatico = require('angular').module('ivamatico.calculador', []);
 ivamatico.config(require('./config.route'));
 ivamatico.controller('CalcularController', require('./calcular.controller'));
 ivamatico.controller('DesglosarController', require('./desglosar.controller'));
+ivamatico.directive('result', require('./result.directive'));
