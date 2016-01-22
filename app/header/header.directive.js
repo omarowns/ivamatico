0e5be83df2888bff954c2e9dfaf819eb0334685a
Omar Garcia <isc.omargarcia@gmail.com>
2016-01-22 00:40:59 -0600

    Hacer uso de las constantes para el titulo y subtitulo de la app

--- app/header/header.directive.js
+++ app/header/header.directive.js
@@ -1,13 +1,22 @@
 'use strict';
 
-HeaderDirective.$inject = [];
+HeaderDirective.$inject = ['APP_CONSTANTS'];
 
-function HeaderDirective() {
+function HeaderDirective(CONSTANTS) {
     var directive = {
         restrict: 'A',
-        template: require('./header.html')
+        template: require('./header.html'),
+        link: link
     };
     return directive;
+
+    function link(scope, element, attrs) {
+        var title = element.find('h1');
+        var subtitle = element.find('h2');
+
+        title.text(CONSTANTS.HEADER_TITLE);
+        subtitle.text(CONSTANTS.HEADER_SUBTITLE);
+    }
 }
 
 module.exports = HeaderDirective;
