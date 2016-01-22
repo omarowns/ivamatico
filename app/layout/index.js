6fe36d04fe230cf3ff20454830265119d59579e8
Omar Garcia <isc.omargarcia@gmail.com>
2016-01-22 00:39:40 -0600

    quitar modulo de layout

--- app/layout/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var ivamatico = require('angular').module('ivamatico.layout', []);
-
-ivamatico.config(require('./config.route'));
-ivamatico.controller('LayoutController', require('./layout.controller'));
